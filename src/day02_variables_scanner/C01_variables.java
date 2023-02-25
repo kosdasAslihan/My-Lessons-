5e3ef44fe382e0cd475e1af553d46fc7485ee7d1
Aslihan Kosdas <kosdasaslihan@gmail.com>
2023-02-25 19:32:35 +0300

    My Lessons

---
+++ src/day02_variables_scanner/C01_variables.java
@@ -0,0 +1,27 @@
+package day02_variables_scanner;
+
+public class C01_variables {
+        /*
+        Java'da kodlarimizi class'larda olustururuz
+        Class'lar ihtiyac duyuldugunda obje olusturmak icin de kullanilir.
+
+        Java'nin baslangic noktasi (entry point) main method'dur.
+
+        variable'ları datalarimizi saklamak (store) icin olustururuz.
+         */
+
+    public static void main(String[] args) {
+
+        System.out.println("Java Candir");
+
+        String             ogrenciIsmi            =                     "Matrix           .";
+        //data turu        variable ismi        assignment(atama)        deger
+
+        System.out.println(ogrenciIsmi); // Matrix
+
+        ogrenciIsmi= "Tayfur";
+
+        System.out.println(ogrenciIsmi); // Tayfur
+
+    }
+}
